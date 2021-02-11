40fbffba77b3cf8c7cf48e12921b3b7715426808
Toluwalase Okuwoga <toluwalaseokuwoga@yahoo.com>
2021-02-11 13:10:59 +0100

    Added my project

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
